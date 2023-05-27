38fbcaa62c28fa6dbdaae9180aa9e11bcfa89fd4
Tami Takada <tamitakada@gmail.com>
2023-05-27 09:10:19 -0400

    tohex

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -4,4 +4,8 @@ extension ColorConverter on Color {
   static Color parse(String hexColor) {
     return Color(int.parse(hexColor, radix: 16) + 0xFF000000);
   }
+
+  String toHex() {
+    return value.toRadixString(16);
+  }
 }
